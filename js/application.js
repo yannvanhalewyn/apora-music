f474892277b4718ffc6bf52ae009282d32a5d770
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-07-28 01:24:10 +0200

    Inline svg icons

--- js/application.js
+++ js/application.js
@@ -1,21 +1,4 @@
-const inlineSVGs = function() {
-  $('img.js-load-svg').each(function() {
-    var el = $(this);
-    var url = el.attr('src');
-    var _class = el.attr('class');
-
-    $.get(url, function(data) {
-      var svgEl = $(data).find('svg');
-      if (typeof _class !== 'undefined') {
-        svgEl = svgEl.attr('class', _class);
-      }
-      svgEl = svgEl.removeAttr('xmlns:a');
-      el.replaceWith(svgEl);
-    }, 'xml');
-  });
-}
-
-const initScrollMenu = function() {
+const init = function() {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
@@ -25,11 +8,6 @@ const initScrollMenu = function() {
       });
     });
   });
-}
-
-const init = function() {
-  inlineSVGs();
-  initScrollMenu();
 }
 
 $(init);
